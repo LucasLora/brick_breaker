03ccb69440c2912b792fc17f2844222d143c2c43
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2025-09-12 22:24:05 -0300

    Change Class Member Location and Name

--- lib/ui/screens/about/about_viewmodel.dart
+++ lib/ui/screens/about/about_viewmodel.dart
@@ -1,33 +1,33 @@
-import 'package:brick_breaker/ui/screens/about/member.dart';
+import 'package:brick_breaker/models/member.dart';
 import 'package:flutter/material.dart';
 
 class AboutViewModel extends ChangeNotifier {
-  final List<Member> _members = [
-    Member(
+  final List<GroupMember> _members = [
+    GroupMember(
       name: 'Gabriel Fachini de Freitas',
       imagePath: 'assets/profile_images/gabriel.png',
     ),
-    Member(
+    GroupMember(
       name: 'Helena Almeida Franco',
       imagePath: 'assets/profile_images/helena.png',
     ),
-    Member(
+    GroupMember(
       name: 'João Augusto Bortolanza',
       imagePath: 'assets/profile_images/joao.png',
     ),
-    Member(
+    GroupMember(
       name: 'Júlio Moreira Leite Neto',
       imagePath: 'assets/profile_images/julio.png',
     ),
-    Member(
+    GroupMember(
       name: 'Lucas Felippi Lóra',
       imagePath: 'assets/profile_images/lucas.png',
     ),
-    Member(
+    GroupMember(
       name: 'Tiago Ribeiro de Barros',
       imagePath: 'assets/profile_images/tiago.png',
     ),
   ];
 
-  List<Member> get members => _members;
+  List<GroupMember> get members => _members;
 }
--- lib/ui/screens/about/member.dart
+++
@@ -1,6 +0,0 @@
-class Member {
-  final String name;
-  final String imagePath;
-
-  Member({required this.name, required this.imagePath});
-}
